b583821f0619ba8b31ab85eca91dbac88100863d
danielsan80 <danilo.sanchi@gmail.com>
2013-05-01 15:25:48 +0200

    Desire generated

---
+++ src/Dan/MainBundle/Entity/Desire.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace Dan\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Desire
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Dan\MainBundle\Entity\DesireRepository")
+ */
+class Desire
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="game_id", type="string", length=255)
+     */
+    private $game_id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createAt", type="datetime")
+     */
+    private $createAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updateAt", type="datetime")
+     */
+    private $updateAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="note", type="text")
+     */
+    private $note;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="reward", type="integer")
+     */
+    private $reward;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set game_id
+     *
+     * @param string $gameId
+     * @return Desire
+     */
+    public function setGameId($gameId)
+    {
+        $this->game_id = $gameId;
+    
+        return $this;
+    }
+
+    /**
+     * Get game_id
+     *
+     * @return string 
+     */
+    public function getGameId()
+    {
+        return $this->game_id;
+    }
+
+    /**
+     * Set createAt
+     *
+     * @param \DateTime $createAt
+     * @return Desire
+     */
+    public function setCreateAt($createAt)
+    {
+        $this->createAt = $createAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get createAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreateAt()
+    {
+        return $this->createAt;
+    }
+
+    /**
+     * Set updateAt
+     *
+     * @param \DateTime $updateAt
+     * @return Desire
+     */
+    public function setUpdateAt($updateAt)
+    {
+        $this->updateAt = $updateAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get updateAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdateAt()
+    {
+        return $this->updateAt;
+    }
+
+    /**
+     * Set note
+     *
+     * @param string $note
+     * @return Desire
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+    
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return string 
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * Set reward
+     *
+     * @param integer $reward
+     * @return Desire
+     */
+    public function setReward($reward)
+    {
+        $this->reward = $reward;
+    
+        return $this;
+    }
+
+    /**
+     * Get reward
+     *
+     * @return integer 
+     */
+    public function getReward()
+    {
+        return $this->reward;
+    }
+}

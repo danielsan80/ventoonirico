2b1b9e1368ef9cdc252901264683fbf585e7dba2
danielsan80 <danilo.sanchi@gmail.com>
2013-09-26 10:13:59 +0200

    rimossi i file commentati

--- src/Dan/MainBundle/Controller/DefaultController.php
+++ src/Dan/MainBundle/Controller/DefaultController.php
@@ -6,20 +6,6 @@ use Dan\CommonBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
-
-use Symfony\Component\HttpFoundation\Response;
-
-//use Doctrine\Common\Cache\FilesystemCache;
-//use Guzzle\Cache\DoctrineCacheAdapter;
-//use Guzzle\Cache\NullCacheAdapter;
-//use Guzzle\Plugin\Cache\CachePlugin;
-
-//use Dan\MainBundle\Entity\Game;
-use Dan\MainBundle\Entity\Desire;
-use Dan\MainBundle\Service\BGGService;
-
-use Symfony\Component\Yaml\Yaml;
 
 /**
  * @Route("") 
